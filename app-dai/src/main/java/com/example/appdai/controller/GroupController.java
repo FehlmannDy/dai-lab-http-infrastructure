7b997eafd4499a3b10148e7fea51e4f6cc70c668
FehlmannDy <dylan13f@gmail.com>
2025-01-15 14:44:01 +0100

    Add GroupController

---
+++ app-dai/src/main/java/com/example/appdai/controller/GroupController.java
@@ -0,0 +1,95 @@
+package com.example.appdai.controller;
+
+import com.example.appdai.model.Artist;
+import com.example.appdai.model.Group;
+import io.javalin.Javalin;
+import org.springframework.stereotype.Component;
+import com.example.appdai.model.Photocard;
+import com.example.appdai.service.PcService;
+
+import java.util.List;
+import java.util.Map;
+import java.time.LocalDate;
+
+/*
+Le package controller regroupe toutes les classes responsables de gérer les requêtes HTTP (les endpoints).
+Chaque controller correspond généralement à une entité ou une ressource spécifique.
+
+Exemple :
+    GroupController : Gère les routes liées aux groupes.
+ */
+@Component
+public class GroupController {
+
+    private final Javalin app;
+    private final GroupService groupService;
+
+    public GroupController(Javalin app, GroupService service) {
+        this.app = app;
+        this.groupService = service;
+    }
+    public void registerRoutes(Javalin app) {
+
+        // GET: la liste de tous les groupes (où proposed = false)
+        app.get("/api/groups", ctx -> {
+            List<Group> allGroups = groupService.getAllGroups();
+            if(allGroups != null && !allGroups.isEmpty()){
+                ctx.status(200).json(allGroups);
+            }else{
+                ctx.status(404).result("Error no group handling");
+            }
+            
+        });
+
+        // GET: tous les groupes proposés
+        app.get("/api/proposed/groups", ctx ->{
+            List<Group> proposedGroups = groupService.getProposedGroups();
+            if (proposedGroups != null && !proposedGroups.isEmpty()) {
+                ctx.status(200).json(proposedGroups);
+            }else{
+                ctx.status(404).result("Error no proposed group handling");
+            }   
+        });
+
+        // INSERT: insert un groupe dand le BDD avec proposed = true
+        app.patch("/api/add/group", ctx ->{
+            Group group = ctx.bodyAsClass(Group.class);
+            if (groupService.addGroup(group)) {
+                ctx.status(201).result("Group added successfully");
+            } else {
+                ctx.status(400).result("Error adding group");
+            }
+        });
+
+        // UPDATE: ajoute le disband date à un groupe
+        app.patch("/api/disband/group", ctx->{
+            Int groupId = Int.parseInt(ctx.body("groupId"));
+            Date disbandDate = Date.parseDate(ctx.body("disbandDate"));
+            if(disbandDate.isBefore(LocalDate.now())){
+                if(groupService.disbandGroup(groupId,disbandDate)){
+                    ctx.status(201).result("Group disband successfully");
+                }else{
+                    ctx.status(400).result("Error disband group");
+                }
+            }else{
+                ctx.status(400).result("Error group can't disband in the future");
+            }
+        });
+
+        // UPDATE: groupe validé par les admins
+        app.patch("/api/admin/group",ctx->{
+            //TODO verifier le tokcen admin
+            Int groupId = Int.parseInt(ctx.body("groupId"));
+            if(groupId != null){
+                if(groupService.validGroup(groupId)){
+                    ctx.status(201).result("Group valid succesfully");
+                }else{
+                    ctx.status(400).result("Error valid group");
+                }
+            }else{
+                ctx.status(400).result("Error groupId is not valid");
+            }
+        });
+    }
+
+}
