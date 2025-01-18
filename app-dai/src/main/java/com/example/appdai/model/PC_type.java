e38e1cf1ca8d0f86438cc9d98d0de849e82211b6
Elbunita <101522406+KurohanaNita@users.noreply.github.com>
2025-01-18 22:25:05 +0100

    Updated some last stuff

---
+++ app-dai/src/main/java/com/example/appdai/model/PC_type.java
@@ -0,0 +1,17 @@
+package com.example.appdai.model;
+
+/**
+ * PC_type Enum
+ * This enum is used to represent the type of Photocard.
+ * A Photocard can be :
+ * - PCA : Photocard from an album
+ * - POB : Photocard from a pre-order benefit or a lucky draw
+ * - PCO : Photocard from an official source
+ * - PCE : Photocard from an event
+ */
+public enum PC_type {
+    PCA,
+    POB,
+    PCO,
+    PCE
+}
--- app-dai/src/main/java/com/example/appdai/model/Photocard.java
+++ app-dai/src/main/java/com/example/appdai/model/Photocard.java
@@ -6,22 +6,6 @@
  */
 package com.example.appdai.model;
 
-/**
- * PC_type Enum
- * This enum is used to represent the type of Photocard.
- * A Photocard can be :
- * - PCA : Photocard from an album
- * - POB : Photocard from a pre-order benefit or a lucky draw
- * - PCO : Photocard from an official source
- * - PCE : Photocard from an event
- */
-enum PC_type {
-    PCA,
-    POB,
-    PCO,
-    PCE
-}
-
 public class Photocard {
 
     private int id;                 // ID unique de la carte
